728a8142b852798df723f975cdda6d7e99368faf
estehbunny <estehbunny@gmail.com>
2022-07-09 16:24:07 +0700

    Migrated to SCSS styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './App.css'
+import './App.scss'
 
 import BigIngredientCard from './components/BigIngredientCard'
 import RecipeList from './components/RecipeList'
